256aef607870f0ac7691889501e48ad641dcd840
Jeremy Akhavi <jeremyakhavi@me.com>
2023-04-04 15:43:19 +0100

    Added voting and region information to MP records

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 import requests
 import json
-from person import Person, MP
+from person import MP
 import os
 from database import Database
+import scraper
+import traceback
+from tqdm import tqdm
 
 API_KEY = 'ByYxD9BjmESCCbdKusCwZCCj'
 
@@ -32,31 +35,60 @@ def mp_data_to_file():
         json.dump(data, f)
     jprint(data)
 
-def parse_mp_data_from_file(filepath):
+def parse_mp_data_from_file(filepath, constituency_region_dict):
     mp_list = []
     with open(filepath, 'r') as f:
         data = json.load(f)
     for person in data:
-        mp = MP(person['name'], person['party'], person['constituency'])
+        region = constituency_region_dict[person['constituency']]
+        mp = MP(person['person_id'], person['name'], person['party'], person['constituency'], region)
         mp_list.append(mp)
     return mp_list
 
-def create_person_work(tx, name, party, constituency):
+def get_region_from_constituency(dict, constituency):
+    return dict[constituency]
+
+def create_person_work(tx, name, party, constituency, region):
     return tx.run("MERGE (m:MP {name: $name}) ON CREATE SET m.constituency = $constituency\
                    MERGE (p:Party {name: $party}) \
+                   MERGE (r:Region {name: $region}) \
                    MERGE (m)-[:IS_A_MEMBER_OF]->(p) \
-                   RETURN m, p",
-        name=name, party=party, constituency=constituency).single()
+                   MERGE (m)-[:REPRESENTS_REGION]->(r) \
+                   RETURN m, p, r",
+        name=name, party=party, constituency=constituency, region=region).single()
 
-def create_person(mp: MP):
-    print(f"Creating node for {mp.name}")
+def create_vote_for_work(tx, name, vote, strength):
+    return tx.run("MATCH (m:MP {name: $name}) \
+                   MERGE (p:Policy {name: $vote}) \
+                   MERGE (m)-[:VOTED_FOR {strength: $strength}]->(p) \
+                   RETURN p",
+                   name=name, vote=vote, strength=strength).single()
+
+def create_vote_against_work(tx, name, vote, strength):
+    return tx.run("MATCH (m:MP {name: $name}) \
+                   MERGE (p:Policy {name: $vote}) \
+                   MERGE (m)-[:VOTED_AGAINST {strength: $strength}]->(p) \
+                   RETURN p",
+                   name=name, vote=vote, strength=strength).single()
+
+def create_person(driver, mp: MP):
+    # print(f"Creating node for {mp.name}")
     # Create a Session for the `people` database
     session = driver.session()
-
     # Create a node within a write transaction
     record = session.execute_write(create_person_work,
-                                    name=mp.name, party=mp.party, constituency=mp.constituency)
+                                    name=mp.name, party=mp.party, constituency=mp.constituency, region=mp.region)
+    voted_for = ['voted for', 'generally voted for', 'consistently voted for']
+    voted_against = ['voted against', 'generally voted against', 'consistently voted against']
+    for vote in mp.votes:
+        # if MP voted in favour of or against the issue then add relationship to graph
+        if vote[1] in voted_for:
+            session.execute_write(create_vote_for_work,
+                                    name=mp.name, vote=vote[0], strength=vote[1])
 
+        elif vote[1] in voted_against:
+            session.execute_write(create_vote_against_work,
+                                    name=mp.name, vote=vote[0], strength=vote[1])
     # Get the `p` value from the first record
     person = record["p"]
 
@@ -66,9 +98,29 @@ def create_person(mp: MP):
     # Return the property from the node
     return person["name"]
 
-driver = Database.init_driver(os.getenv("NEO4J_URI"), os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
+def main():
+    driver = Database.init_driver(os.getenv("NEO4J_URI"), os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
 
-mp_list = parse_mp_data_from_file('mp-data.json')
-for mp in mp_list:
-    create_person(mp)
+    constituency_region_dict = scraper.scrape_constituency_regions()
+    if constituency_region_dict is None:
+        print('Error getting constituency - region mapping')
+    mp_list = parse_mp_data_from_file('mp-data.json', constituency_region_dict)
+    print(len(mp_list))
+    for mp in tqdm(mp_list):
+        try:
+            scraper.scrape_mp_votes(mp)
+        except Exception:
+            traceback.print_exc()
+        finally:
+            try:
+                create_person(driver, mp)
+            except Exception:
+                traceback.print_exc()
+
+    for mp in mp_list:
+        print(vars(mp))
+
+if __name__ == '__main__':
+    main()
+
 
--- person.py
+++ person.py
@@ -1,14 +1,15 @@
 import abc
 
 class Person():
-    def __init__(self, name, party, constituency):
+    def __init__(self, id, name, party, constituency, region):
+        self.id = id
         self.name = name
         self.party = party
         self.constituency = constituency
+        self.votes = []
+        self.region = region
 
 class MP(Person):
     def describe_mp(self):
         description = f"Hello, I am {self.name} of the {self.party} party, representing the constituents of {self.constituency}"
-        if self.party == "Conservative":
-            description += " and I'm a CUNT"
         print(description)
