c41535f21cdedd4a673f33f5f5c269b99d62ace0
Jeremy Akhavi <jeremyakhavi@me.com>
2023-04-06 18:07:48 +0100

    Restructured data collection to include extra information

--- main.py
+++ main.py
@@ -7,23 +7,13 @@ from database import Database
 import scraper
 import traceback
 from tqdm import tqdm
+from dotenv import load_dotenv
 
 API_KEY = 'ByYxD9BjmESCCbdKusCwZCCj'
 
-def twfy_get(payload):
-    """
-    Submit get request to TheyWorkForYou (twfy) and return response
-    """
-    service_url = 'https://www.theyworkforyou.com/api/'
-    payload['key'] = API_KEY
-    payload['output'] = 'json'
-
-    response = requests.get(service_url, params=payload)
-    return response
-
 def jprint(obj):
     """
-    Create nicely formatted strong of Python JSON object
+    Create nicely formatted string of Python JSON object
     """
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
@@ -44,9 +34,6 @@ def parse_mp_data_from_file(filepath, constituency_region_dict):
         mp = MP(person['person_id'], person['name'], person['party'], person['constituency'], region)
         mp_list.append(mp)
     return mp_list
-
-def get_region_from_constituency(dict, constituency):
-    return dict[constituency]
 
 def create_person_work(tx, name, party, constituency, region):
     return tx.run("MERGE (m:MP {name: $name}) ON CREATE SET m.constituency = $constituency\
@@ -98,15 +85,99 @@ def create_person(driver, mp: MP):
     # Return the property from the node
     return person["name"]
 
+def get_mps_from_members_api():
+    print('Getting members')
+    # Set up the API endpoint and request parameters
+    url = 'https://members-api.parliament.uk/api/Members/Search'
+    params = {'take': 20, 'skip': 0, 'IsCurrentMember': True, 'House': 1}
+
+    mp_dict = {}
+
+    # Loop through each page of results until we've retrieved all MPs
+    while True:
+        # Make the API request with the current pagination parameters
+        response = requests.get(url, params=params)
+        # Check for errors in the response
+        if response.status_code != 200:
+            print(f'Get Members API request failed with code: {response.status_code}')
+            time.sleep(5)
+            continue
+        # Parse the response data as JSON
+        data = response.json()
+
+        # Extract the MPs from the response and add them to our list
+        for mp in data['items']:
+            mp_obj = MP(id=mp['value']['id'], party=mp['value']['latestParty']['name'], 
+                        constituency=mp['value']['latestHouseMembership']['membershipFrom'],
+                        gender=mp['value']['gender'],
+                        start_date=mp['value']['latestHouseMembership']['membershipStartDate'])
+            
+            mp_dict[mp_obj.constituency] = mp_obj
+
+        # Check if we've retrieved all MPs
+        if len(mp_dict) == data['totalResults']:
+            break
+
+        # Update the pagination parameters for the next API request
+        params['skip'] += params['take']
+        print(params)
+    
+    return mp_dict
+
+def get_twfy_ids():
+    """
+    Submit get request to TheyWorkForYou (twfy)
+    Extract relevant ids and names from response and put in dictionary
+    Return dictionary
+    """
+    print('Getting twfy ids')
+    twfy_dict = {}
+
+    url = 'https://www.theyworkforyou.com/api/getMPs'
+    params = {'key': API_KEY, 'output': 'json'}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    for mp in data:
+        constituency = mp['constituency'].lower()
+        twfy_dict[constituency] = {'name': mp['name'], 'twfy_id': mp['person_id']}
+
+    return twfy_dict
+
+def get_govt_posts_from_members_api():
+    print('Getting government posts')
+    url = 'https://members-api.parliament.uk/api/Posts/GovernmentPosts'
+    govt_post_dict = {}
+    response = requests.get(url)
+    data = response.json()
+    for govt_post in data:
+        mp_id = govt_post['value']['postHolders'][0]['member']['value']['id']
+        post_name = govt_post['value']['name']
+        govt_post_dict[mp_id] = post_name
+    
+    return govt_post_dict
+
 def main():
+    # load environment variables from .env file
+    load_dotenv()
     driver = Database.init_driver(os.getenv("NEO4J_URI"), os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
 
     constituency_region_dict = scraper.scrape_constituency_regions()
     if constituency_region_dict is None:
         print('Error getting constituency - region mapping')
-    mp_list = parse_mp_data_from_file('mp-data.json', constituency_region_dict)
-    print(len(mp_list))
-    for mp in tqdm(mp_list):
+
+    twfy_dict = get_twfy_ids()
+    govt_post_dict = get_govt_posts_from_members_api()
+
+    mp_dict = get_mps_from_members_api()
+    
+    for mp in tqdm(mp_dict.values()):       
+        mp.set_region(constituency_region_dict)
+        mp.set_twfy_id_name(twfy_dict)
+        mp.set_election_result()
+        
+        if mp.id in govt_post_dict:
+            mp.set_govt_post(govt_post_dict[mp.id])
         try:
             scraper.scrape_mp_votes(mp)
         except Exception:
@@ -116,9 +187,24 @@ def main():
                 create_person(driver, mp)
             except Exception:
                 traceback.print_exc()
+    
 
-    for mp in mp_list:
-        print(vars(mp))
+    
+    # mp_list = parse_mp_data_from_file('mp-data.json', constituency_region_dict)
+    # print(len(mp_list))
+    # for mp in tqdm(mp_list):
+    #     try:
+    #         scraper.scrape_mp_votes(mp)
+    #     except Exception:
+    #         traceback.print_exc()
+    #     finally:
+    #         try:
+    #             create_person(driver, mp)
+    #         except Exception:
+    #             traceback.print_exc()
+
+    # for mp in mp_list:
+    #     print(vars(mp))
 
 if __name__ == '__main__':
     main()
--- person.py
+++ person.py
@@ -1,15 +1,48 @@
-import abc
+import requests
 
-class Person():
-    def __init__(self, id, name, party, constituency, region):
+class MP:
+    def __init__(self, id, party, constituency, gender, start_date):
         self.id = id
-        self.name = name
+        self.twfy_id = None
         self.party = party
-        self.constituency = constituency
+        self.constituency = constituency.lower()
+        self.region = None
         self.votes = []
-        self.region = region
+        self.gender = gender
+        self.start_date = start_date
+        self.name = None
+        self.electorate = None
+        self.turnout = None
+        self.majority = None
+        self.govt_post = None
 
-class MP(Person):
-    def describe_mp(self):
-        description = f"Hello, I am {self.name} of the {self.party} party, representing the constituents of {self.constituency}"
-        print(description)
+    def __str__(self):
+        return f"MP object: id={self.id}\ntwfy_id={self.twfy_id}\nparty={self.party}\nconstituency={self.constituency}\n\
+                gender={self.gender}\nstart_date={self.start_date}\nname={self.name}\n\
+                electorate={self.electorate}\nturnout={self.turnout}\nmajority={self.majority}\nvotes={self.votes}"
+
+    def set_election_result(self):
+        url = f'https://members-api.parliament.uk/api/Members/{self.id}/LatestElectionResult'
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise Exception(f'API request failed with response code {response.status_code}')
+        
+        data = response.json()
+
+        self.electorate = data['value']['electorate']
+        self.turnout = data['value']['turnout']
+        self.majority = data['value']['majority']
+
+    def set_twfy_id_name(self, twfy_dict):
+        self.name = twfy_dict[self.constituency]['name']
+        self.twfy_id = twfy_dict[self.constituency]['twfy_id']
+
+    def set_region(self, constituency_region_dict):
+        self.region = constituency_region_dict[self.constituency]
+
+    def set_govt_post(self, govt_post):
+        self.govt_post = govt_post
+
+    def set_votes(self, votes):
+        self.votes = votes
